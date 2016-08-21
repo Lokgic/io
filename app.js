2c1f66593ea710f2e81c060e579597a4547b3916
Lokgic <lok.chan@duke.edu>
2016-08-20 22:21:33 -0400

    added mLab function

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 //mongodb connection
 
 
-mongoose.connect("mongodb://localhost:27017/lokgic")
+mongoose.connect("mongodb://lokgic:WAuS8Pi4X37y4.mlab.com:13216/heroku_7j1n343l/lokgic")
 var db = mongoose.connection;
 
 //mongo error
