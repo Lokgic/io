572dc009492f56d90b32c0cdbb17a56a5e37f1b6
Lokgic <lok.chan@duke.edu>
2016-08-26 01:26:18 -0400

    added close pdf button

---
+++ public/javascripts/closePDFButton.js
@@ -0,0 +1,11 @@
+$(function(){
+	var	$button = $('#closepdf');
+	var $pdf = $('#right');
+	var $rightCol = $('#rightCol');
+	var $leftCol = $('left');
+	$button.on('click', function(){
+		$rightCol.remove();
+
+	}) 
+
+})
