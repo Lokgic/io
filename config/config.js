fca2df9e4a4de64a5ddf845f9da05a51af694b0a
Lokgic <lok.chan@duke.edu>
2016-10-02 12:59:03 -0400

    config

---
+++ config/config.js
@@ -0,0 +1,8 @@
+var config = {
+  mongo: {
+    url: "mongodb://lokgicio:ZqA7yQ3kZyUiV@ds021986-a0.mlab.com:21986,ds021986-a1.mlab.com:21986/lokgicio?replicaSet=rs-ds021986"
+  }
+}
+
+
+module.exports = config;
