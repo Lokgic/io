665e2c5c1143f3b3e0f7c00e35d6e7956deb65e6
lokgic <loklokj@gmail.com>
2016-12-24 22:41:53 -0500

    working on data viz

--- public/javascripts/syl.js
+++ public/javascripts/syl.js
@@ -1,12 +1,12 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// https://d3js.org Version 4.2.5. Copyright 2016 Mike Bostock.
+// https://d3js.org Version 4.2.6. Copyright 2016 Mike Bostock.
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (factory((global.d3 = global.d3 || {})));
 }(this, (function (exports) { 'use strict';
 
-var version = "4.2.5";
+var version = "4.2.6";
 
 var ascending = function(a, b) {
   return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
@@ -5098,9 +5098,9 @@ function newInterval(floori, offseti, count, field) {
 
   interval.filter = function(test) {
     return newInterval(function(date) {
-      while (floori(date), !test(date)) date.setTime(date - 1);
+      if (date >= date) while (floori(date), !test(date)) date.setTime(date - 1);
     }, function(date, step) {
-      while (--step >= 0) while (offseti(date, 1), !test(date)) {} // eslint-disable-line no-empty
+      if (date >= date) while (--step >= 0) while (offseti(date, 1), !test(date)) {} // eslint-disable-line no-empty
     });
   };
 
@@ -16288,58 +16288,108 @@ Object.defineProperty(exports, '__esModule', { value: true });
 var d3 = require('d3')
 
 
-var scrollTop = 0
-var newScrollTop = 0
 
-var container = d3.select('#con')
-var panel = d3.selectAll('.panel')
-var sticky = d3.select('#sticky')
+var root = {
+  "name": "overview",
+  "children":[
+    {
+      "name": "Sentential Logic",
+      "children":[
+        {"name": "Exercises",
+          "children":[
 
-var WIDTH = window.innerWidth
- var HEIGHT = window.innerHeight
-
-
-var svg = d3.select('#sticky')
-        .append("svg")
-svg.attr("width", WIDTH)
-    .attr("height", HEIGHT)
-
-// circle = svg.append("circle")
-//
-// circle.attr("cx", "50%")
-//       .attr("cy", "50%")
-//       .attr("r", 30)
-//
-
-line = svg.append("line")
-line.attr("x1", 121)
-    .attr("y1", HEIGHT/2)
-    .attr("x2", 152)
-    .attr("y2", HEIGHT/2)
-    .attr("stroke-width","2")
-    .attr("stroke","black")
-
-
-container.on("scroll.scroller", function() {
-    newScrollTop = container.node().scrollTop
-
-    // console.log("works?")
-  });
-
-
-
-  var render = function() {
-    // Don't re-render if scroll didn't change
-    if (scrollTop !== newScrollTop) {
-      scrollTop = newScrollTop
-      // Graphics Code Goes Here
-      x = parseInt(line.attr('x2'));
-      console.log("x " +x)
-      line.attr('x2', scrollTop)
+          ]},
+        {"name": "Quizzes", "size":2},
+        {"name": "Assignments", "size":3},
+        {"name": "Test", "size": 1}
+      ]
+    },
+    {
+      "name": "Predicate Logic",
+      "children":[
+        {"name": "Exercises", "size":1},
+        {"name": "Quizzes", "size":2},
+        {"name": "Assignments", "size":1},
+        {"name": "Test", "size": 4}
+      ]
+    },
+    {
+      "name": "Natural Deduction",
+      "children":[
+        {"name": "Exercises", "size":7},
+        {"name": "Quizzes", "size":2},
+        {"name": "Assignments", "size":3},
+        {"name": "Test",
+        "children":[
+          {"name": "t", "size": 1}
+        ]}
+      ]
+    },
+    {
+      "name": "Identity",
+      "children":[
+        {"name": "Exercises", "size":3},
+        {"name": "Quizzes", "size":3},
+        {"name": "Assignments", "size":5},
+        {"name": "Test", "size": 5}
+      ]
     }
-    window.requestAnimationFrame(render)
-  }
+  ]
+}
 
-  window.requestAnimationFrame(render)
+
+
+
+var width = 960,
+    height = 700,
+    radius = (Math.min(width, height) / 2) - 10;
+
+var formatNumber = d3.format(",d");
+
+var x = d3.scaleLinear()
+    .range([0, 2 * Math.PI]);
+
+var y = d3.scaleSqrt()
+    .range([0, radius]);
+
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+var partition = d3.partition();
+
+var arc = d3.arc()
+    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
+    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
+    .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
+    .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
+
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+
+    root = d3.hierarchy(root);
+    root.sum(function(d) { return d.size; });
+    svg.selectAll("path")
+        .data(partition(root).descendants())
+      .enter().append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color((d.children ? d : d.parent).data.name); })
+
+
+//
+// var colorCircle = svg.append('g')
+// .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+//
+//
+// var path = colorCircle.selectAll('path')
+//   .data(pie(dataset))
+//   .enter()
+//   .append('path')
+//   .attr('d', arc)
+//   .attr('fill', function(d, i) {
+//     return color(d.data.label);
+//   });
 
 },{"d3":1}]},{},[2]);
--- public/javascripts/syl_s.js
+++ public/javascripts/syl_s.js
@@ -1,56 +1,106 @@
 var d3 = require('d3')
 
 
-var scrollTop = 0
-var newScrollTop = 0
 
-var container = d3.select('#con')
-var panel = d3.selectAll('.panel')
-var sticky = d3.select('#sticky')
+var root = {
+  "name": "overview",
+  "children":[
+    {
+      "name": "Sentential Logic",
+      "children":[
+        {"name": "Exercises",
+          "children":[
 
-var WIDTH = window.innerWidth
- var HEIGHT = window.innerHeight
-
-
-var svg = d3.select('#sticky')
-        .append("svg")
-svg.attr("width", WIDTH)
-    .attr("height", HEIGHT)
-
-// circle = svg.append("circle")
-//
-// circle.attr("cx", "50%")
-//       .attr("cy", "50%")
-//       .attr("r", 30)
-//
-
-line = svg.append("line")
-line.attr("x1", 121)
-    .attr("y1", HEIGHT/2)
-    .attr("x2", 152)
-    .attr("y2", HEIGHT/2)
-    .attr("stroke-width","2")
-    .attr("stroke","black")
-
-
-container.on("scroll.scroller", function() {
-    newScrollTop = container.node().scrollTop
-
-    // console.log("works?")
-  });
-
-
-
-  var render = function() {
-    // Don't re-render if scroll didn't change
-    if (scrollTop !== newScrollTop) {
-      scrollTop = newScrollTop
-      // Graphics Code Goes Here
-      x = parseInt(line.attr('x2'));
-      console.log("x " +x)
-      line.attr('x2', scrollTop)
+          ]},
+        {"name": "Quizzes", "size":2},
+        {"name": "Assignments", "size":3},
+        {"name": "Test", "size": 1}
+      ]
+    },
+    {
+      "name": "Predicate Logic",
+      "children":[
+        {"name": "Exercises", "size":1},
+        {"name": "Quizzes", "size":2},
+        {"name": "Assignments", "size":1},
+        {"name": "Test", "size": 4}
+      ]
+    },
+    {
+      "name": "Natural Deduction",
+      "children":[
+        {"name": "Exercises", "size":7},
+        {"name": "Quizzes", "size":2},
+        {"name": "Assignments", "size":3},
+        {"name": "Test",
+        "children":[
+          {"name": "t", "size": 1}
+        ]}
+      ]
+    },
+    {
+      "name": "Identity",
+      "children":[
+        {"name": "Exercises", "size":3},
+        {"name": "Quizzes", "size":3},
+        {"name": "Assignments", "size":5},
+        {"name": "Test", "size": 5}
+      ]
     }
-    window.requestAnimationFrame(render)
-  }
+  ]
+}
 
-  window.requestAnimationFrame(render)
+
+
+
+var width = 960,
+    height = 700,
+    radius = (Math.min(width, height) / 2) - 10;
+
+var formatNumber = d3.format(",d");
+
+var x = d3.scaleLinear()
+    .range([0, 2 * Math.PI]);
+
+var y = d3.scaleSqrt()
+    .range([0, radius]);
+
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+var partition = d3.partition();
+
+var arc = d3.arc()
+    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
+    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
+    .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
+    .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
+
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+
+    root = d3.hierarchy(root);
+    root.sum(function(d) { return d.size; });
+    svg.selectAll("path")
+        .data(partition(root).descendants())
+      .enter().append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color((d.children ? d : d.parent).data.name); })
+
+
+//
+// var colorCircle = svg.append('g')
+// .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+//
+//
+// var path = colorCircle.selectAll('path')
+//   .data(pie(dataset))
+//   .enter()
+//   .append('path')
+//   .attr('d', arc)
+//   .attr('fill', function(d, i) {
+//     return color(d.data.label);
+//   });
