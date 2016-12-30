a641e81e24b69c8be448e9cf764dcaf47719d5cd
lokgic <loklokj@gmail.com>
2016-12-29 22:18:53 -0500

    m1s1c1

--- public/javascripts/vennD.js
+++ public/javascripts/vennD.js
@@ -5174,6 +5174,8 @@ var bobLabel,
     bobText,
     bowed,
     bowedText,
+    guitarCircle,
+    guitarText,
     plucked,
     pluckedtext,
     height,
@@ -5244,15 +5246,31 @@ $(function(){
                       .clamp(true)
      lineYScale = d3.scaleLinear()
                       .domain([tp('#t1'),tp('#t2')])
-                      .range([bobY, bobY - radius])
+                      .range([bobY, bobY - 1.2*radius])
                       .clamp(true)
       lineOScale = d3.scaleLinear()
-                       .domain([tp('#t2'),tp('#t3')-300])
+                       .domain([tp('#t2'),tp('#t3')-250])
                        .range([1, 0])
                        .clamp(true)
+     p2TextOScale = d3.scaleLinear()
+                      .domain([tp('#t3'),tp('#t4')-250])
+                      .range([0, 0.8])
+                      .clamp(true)
+    p2LineYScale = d3.scaleLinear()
+                     .domain([tp('#t3'),tp('#t4')])
+                     .range([bobY - 1.2*radius, bobY - radius/5])
+                     .clamp(true)
+   p2LineXScale = d3.scaleLinear()
+                    .domain([tp('#t3'),tp('#t4')])
+                    .range([width*0.7, width/2+radius])
+                    .clamp(true)
+    cLineYScale = d3.scaleLinear()
+                     .domain([tp('#t3'),tp('#t4')])
+                     .range([bobY + 1.4*radius, bobY])
+                     .clamp(true)
      bobNameScale = d3.scaleLinear()
                       .domain([tp('#t1'),tp('#t2'),tp('#t3')-300])
-                      .range([0, 1,0])
+                      .range([0, 0.8,0])
                       .clamp(true)
       bobScale = d3.scaleLinear()
                        .domain([tp('#t1'),tp('#t2')])
@@ -5295,9 +5313,9 @@ $(function(){
     function graphicInit(){
 
         bowedCircle = svg.append('g').attr('class','bigCircles').attr('id','bowedCircle').append("circle").attr("r",radius).attr("cx",bowedX).attr("cy", bowedY).style('fill', bowedColor)
-        bowedText =  svg.append('text').attr('class','bigCircles').text('Bowed').attr("dx",bowedX - 40).attr("dy", bowedY).style('fill','black')
+        bowedText =  svg.append('text').attr('class','bigCircles').text('Bowed').attr("dx",bowedX - 40).attr("dy", bowedY + 1.2*radius).style('fill','black')
         pluckedCircle = svg.append('g').attr('class','bigCircles').attr('id','pluckedCircle').append("circle").attr("r",radius).attr("cx",pluckedX).attr("cy", pluckedY).style('fill',pluckedColor)
-        pluckedText = svg.append("text").attr('class','bigCircles').text("Plucked").attr("dx",pluckedX - 30).attr("dy", pluckedY).style('fill','black')
+        pluckedText = svg.append("text").attr('class','bigCircles').text("Plucked").attr("dx",pluckedX - 30).attr("dy", pluckedY + 1.2*radius).style('fill','black')
         d3.selectAll('.bigCircles').transition().style('opacity', 0)
         bobLine = svg.append('g').append('line')
                     .attr('class', 'bobLabel').
@@ -5306,7 +5324,7 @@ $(function(){
                       .attr("y1",bobY)
                       .attr("x2", bobX)
                       .attr("y2",bobY )
-                      .attr("stroke-width", 1)
+                      .attr("stroke-width", 2)
                       .attr("stroke","black")
         bobText = svg.append('text')
           .text("Premise 1: Bob plays a bowed instrument.")
@@ -5315,12 +5333,26 @@ $(function(){
           .attr("dx",bobX - 40)
           .attr("dy", bobY - radius-40)
           .style('opacity',0)
-        svg.append("g").append("circle").attr('id', 'bob').attr("r",radius/8).attr("cx",bobX).attr("cy",bobY ).style('fill', bobColor).attr('opacity', 0)
+        bobCircle = svg.append("g").append("circle").attr('id', 'bob').attr("r",radius/8).attr("cx",bobX).attr("cy",bobY ).style('fill', bobColor).attr('opacity', 0)
+        p2text = svg.append('text')
+                    .text('Premise 2: no guitar is bowed.').attr("dx",width/2).attr("dy", bobY - 1.3*radius).style('fill','black').style('opacity',0)
+        b = [{x: width*0.7 ,y: bobY - 1.2*radius},{x: width*0.7, y: bobY - 1.2*radius}]
+
+        line = d3.line().x(function(d){return d.x}).y(function(d){return d.y})
+        p2line = d3.select('#m1c1s1 svg').append('path').attr('d',line(b)).attr('stroke', 'black').attr('stroke-width','2').attr('fill-opacity', 0)
+        // bline = d3.line().x(function(c){return d.x}).y(function(d){return d.y})
+        conclusionText = svg.append('text')
+                    .text('Conclusion: Bob does not play the guitar.').attr("dx",width/2 - radius).attr("dy", bobY + 1.5*radius ).style('fill','black').style('opacity',0)
+        c = [{x:width/2 ,y: bobY + 1.4*radius}]
+        //
+        // line = d3.line().x(function(d){return d.x}).y(function(d){return d.y})
+        cline = d3.select('#m1c1s1 svg').append('path').attr('d',line(c)).attr('stroke', 'black').attr('stroke-width','2').attr('fill-opacity', 0)
+
 
 
       guitarCircle = svg
                 .append("g").attr('id','guitarStuff').attr('opacity',0)
-                .append("circle").attr("r",radius/3).attr("cx",width/2).attr("cy", bobY).style('fill',guitarColor).attr('id', 'guitar')
+                .append("circle").attr("r",radius/3).attr("cx",width/2).attr("cy", bobY).style('fill',guitarColor)
       guitarText = d3.select('#guitarStuff').append("text").text("Guitar").attr("dx",width/2).attr("dy", bobY).style('fill','black')
 
 
@@ -5332,19 +5364,22 @@ $(function(){
       sticky()
       d3.selectAll('.bigCircles circle').attr('r', radius)
       bowedCircle.attr('r', radius).attr("cx",bowedX).attr("cy", bowedY)
-      bowedText.attr("dx",bowedX - 30).attr("dy", bowedY)
+      bowedText.attr("dx",bowedX - 30).attr("dy", bowedY + 1.2*radius)
       pluckedCircle.attr('r', radius).attr("cx",pluckedX).attr("cy", pluckedY)
-      pluckedText.attr("dx",pluckedX - 40).attr("dy", pluckedY)
+      pluckedText.attr("dx",pluckedX - 40).attr("dy", pluckedY + 1.2*radius )
         bobLine .attr("x1", bobX)
                  .attr("y1",bobY)
                  .attr("x2", bobX)
                   .attr("y2", lineYScale(scrollTop))
       bobText.attr("dx",bobX - 40)
         .attr("dy", bobY - radius-40)
-      guitarText.attr('dy', bobY)
-      guitarCircle.attr('cy', bobY)
+      guitarText.attr('dy', bobY).attr("dx",width/2)
+      // console.log(guitarCircle)
+      guitarCircle.attr('cy', bobY).attr("r",radius/3).attr("cx",width/2)
+        bobCircle.attr("r",radius/8)
       graphics()
       chart.width(width)
+      // console.log(guitarCircle)
     }
 
     function setSVGsize(){
@@ -5356,29 +5391,32 @@ $(function(){
   graphicInit();
 // console.log(freezePoint)
 
-
-
-
   function graphics(){
+    b = [{x: width*0.7, y: bobY - 1.2*radius},{x: p2LineXScale(scrollTop) , y: p2LineYScale(scrollTop)}]
+    c = [{x:width/2 ,y: bobY + 1.4*radius},{x:width/2  ,y: cLineYScale(scrollTop) }]
+    p2line.transition().duration(300).attr('d',line(b))
+    cline.transition().duration(300).attr('d',line(c))
     // console.log(firstScale(scrollTop))
-    d3.selectAll('.bigCircles').transition().duration(500).style('opacity', firstScale(scrollTop))
+    p2text.transition().duration(300).style('opacity',p2TextOScale(scrollTop))
+    conclusionText.transition().duration(300).style('opacity',p2TextOScale(scrollTop))
+    d3.selectAll('.bigCircles').transition().duration(300).style('opacity', firstScale(scrollTop))
    d3.select("#bobLine")
                  .attr("x1", bobX)
                  .attr("y1",bobY)
                  .attr("x2", bobX)
                   .attr("y2", lineYScale(scrollTop))
-    d3.select('#bobName')
+    d3.select('#bobName').transition().duration(300)
                   .style('opacity',bobNameScale(scrollTop))
-    d3.selectAll('#bobLine')
+    d3.selectAll('#bobLine').transition().duration(300)
       .style('opacity', lineOScale(scrollTop))
 
       // console.log("ls" + lineOScale(scrollTop))
-   d3.select('#bob')
+   d3.select('#bob').transition().duration(300)
             .style('opacity',bobScale(scrollTop))
             .attr('cx',bobXScale(scrollTop))
             .attr('cy',bobY)
     d3.select('#guitarStuff').style('opacity',guitarOScale(scrollTop))
-    d3.select('#guitarStuff').attr("transform",'translate(' + guitarXScale(scrollTop) + ')')
+    d3.select('#guitarStuff').transition().duration(300).attr("transform",'translate(' + guitarXScale(scrollTop) + ')')
 
   }
 //second animation
@@ -5396,7 +5434,7 @@ $(function(){
   vennD = d3.select("#m1c1s2 .animation").datum(sets).call(chart)
   $('path','g[data-venn-sets = "Plucked"]').css('fill', pluckedColor).css('fill-opacity',0.7).css('stroke', pluckedColor).css('stroke-opacity',0.7).css("stroke-width", 1)
   $('path','g[data-venn-sets = "Guitar"]').css('fill', guitarColor).css('fill-opacity',0.7).css('stroke', guitarColor).css('stroke-opacity',0.7)
-  console.log(vennD.selectAll('path'))
+  // console.log(vennD.selectAll('path'))
 
   // vennD = d3.select("#m1c1s2 .animation").datum(sets).call(chart)
 
@@ -5405,7 +5443,7 @@ $(function(){
     sets = [ {sets: ['Plucked'], size: 12},
                  {sets: ['Guitar'], size: 6},
                  {sets: ['Plucked','Guitar'], size: vennEulerScale(scrollTop)}];
-                 console.log(vennOScale(scrollTop))
+                //  console.log(vennOScale(scrollTop))
     vennD = d3.select("#m1c1s2 .animation").datum(sets).call(chart)
      d3.selectAll("#m1c1s2 .venn-circle path").style('fill-opacity', vennOScale(scrollTop)).style("stroke-width", vennSScale(scrollTop))
 
