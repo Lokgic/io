177aa9941d8a17b046cf3df61a52e5214d1bb40b
lokgic <loklokj@gmail.com>
2017-03-01 04:00:23 -0500

    alientree gen

--- js/alienTree_gen.js
+++ js/alienTree_gen.js
@@ -1,22 +1,30 @@
 var Chance = require('chance')
 var chance = new Chance();
 var _ = require('underscore');
+var constants = "abcdefghijklmnopqrst".split('')
+var color = ["blue","green","yellow","red","black","grey"]
 
-
-
-module.exports.test = function(x){
-  var table = [
-    {"name": "Eve",   "parent": ""},
-    {"name": "Cain",  "parent": "Eve"},
-    {"name": "Seth",  "parent": "Eve"},
-    {"name": "Enos",  "parent": "Seth"},
-    {"name": "Noam",  "parent": "Seth"},
-    {"name": "Abel",  "parent": "Eve"},
-    {"name": "Awan",  "parent": "Eve"},
-    {"name": "Enoch", "parent": "Awan"},
-    {"name": "Azura", "parent": "Eve"}
-  ]
-
-
-    return table
+var Alien = function(name, color, parent,constant){
+  this.name = name;
+  this.parent = parent;
+  this.color = color;
+  this.constant = constant;
 }
+
+
+function makeAliens(n){
+  var namePool = []
+  while (namePool.length < n){
+    var temp = chance.last()
+    if (!_.contains(namePool,temp)) namePool.push(temp)
+  }
+  var constantPool = chance.pickset(constants,n)
+
+  var output = []
+  while (namePool.length > 0){
+    output.push(new Alien(namePool.pop(),chance.pickone(color),null,constantPool.pop()))
+  }
+  return output
+}
+
+console.log(makeAliens(5))
